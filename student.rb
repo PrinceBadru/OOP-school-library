7ff7f54d1868896ddf4dd7ea02ffce786f9ab385
Badru Ndawula <badrundawula24@gmail.com>
2023-12-18 21:36:15 +0300

    add student review fix

--- student.rb
+++ student.rb
@@ -1,12 +1,14 @@
 require_relative 'Person'
 
 class Student < Person
-  def play_hooky
-    '¯\\(ツ)/¯'
+    def initialize(age, classroom, name = "Unknown", parent_permission = true)
+      super(name: name, age: age, parent_permission: parent_permission)
+      @classroom = classroom
+    end
+  
+    def play_hooky
+      "¯\\(ツ)/¯"
+    end
   end
+  
 
-  def initialize(classroom:, name: 'Unknown', age: 0, parent_permission: true)
-    super(name: name, age: age, parent_permission: parent_permission)
-    @classroom = classroom
-  end
-end
