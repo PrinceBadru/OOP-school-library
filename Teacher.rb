d267588016c398061a2b1710473dfd444a85a88f
Badru Ndawula <badrundawula24@gmail.com>
2023-12-18 20:31:48 +0300

    add Teacher file

---
+++ Teacher.rb
@@ -0,0 +1,12 @@
+require_relative 'Person'
+
+class Teacher < Person
+  def initialize(name = "Unknown", age = 0, specialization)
+    super(name, age)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
