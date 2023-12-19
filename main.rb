74ca79a1e0035520bf99ab86029704f6198d06b6
Badru Ndawula <badrundawula24@gmail.com>
2023-12-19 16:22:48 +0300

    fix linter errors

--- main.rb
+++ main.rb
@@ -11,3 +11,16 @@ puts "Capitalized Name: #{capitalized_person.correct_name}"
 
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts "Capitalized and Trimmed Name: #{capitalized_trimmed_person.correct_name}"
+require_relative 'person'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+# See decorators in action
+person = Person.new(22, 'maximilianus')
+puts "Original Name: #{person.correct_name}"
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts "Capitalized Name: #{capitalized_person.correct_name}"
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts "Capitalized and Trimmed Name: #{capitalized_trimmed_person.correct_name}"
--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name: 'Unknown', parent_permission: true)
+    super() # this line to calls the initialize method of the parent class
     @id = Random.rand(1..1000)
     @age = age
     @name = name
@@ -23,19 +26,3 @@ class Person
     rand(1_000_000..9_999_999)
   end
 end
-
-require_relative 'nameable'
-
-class Person < Nameable
-  attr_reader :name
-
-  def initialize(age, name)
-    @age = age
-    @name = name
-  end
-
-  def correct_name
-    @name
-  end
-end
-
