271417c8d9c9142597fde62f7103bcadd2c816cb
Badru Ndawula <badrundawula24@gmail.com>
2023-12-19 16:34:27 +0300

    fix linters 2

--- main.rb
+++ main.rb
@@ -11,9 +11,6 @@ puts "Capitalized Name: #{capitalized_person.correct_name}"
 
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts "Capitalized and Trimmed Name: #{capitalized_trimmed_person.correct_name}"
-require_relative 'person'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
 
 # See decorators in action
 person = Person.new(22, 'maximilianus')
