07729efb74e29c3a746fc1254f2ed219ab6827aa
Badru Ndawula <badrundawula24@gmail.com>
2023-12-20 18:32:00 +0300

    add rental funcs

---
+++ rental.rb
@@ -0,0 +1,14 @@
+# rental.rb
+
+class Rental
+    attr_reader :date, :book, :person
+  
+    def initialize(date, book, person)
+      @date = date
+      @book = book
+      @person = person
+    end
+  
+    # Additional methods, if needed
+  end
+  
