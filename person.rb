c59e8d0c9d2ad7fbc543b700836a2e141d3d2b6d
Badru Ndawula <badrundawula24@gmail.com>
2023-12-20 18:31:49 +0300

    add person func

--- person.rb
+++ person.rb
@@ -1,32 +1,26 @@
+# person.rb
+
 require_relative 'nameable'
 
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
-    super() # this line to calls the initialize method of the parent class
+  @@all_people = []  # Class variable to store all people
+
+  def initialize(age:, name: 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @@all_people << self  # Add the person to the list when created
   end
 
-  def can_use_services?
-    of_age? || @parent_permission
+  # Class method to retrieve all people
+  def self.all
+    @@all_people
   end
 
-  def correct_name
-    name
-  end
-
-  private
-
-  def of_age?
-    @age >= 18
-  end
-
-  def generate_id
-    rand(1_000_000..9_999_999)
-  end
+  # Additional methods, if needed
 end
