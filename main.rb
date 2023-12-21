ad6d7f22fafa4760868912ef8273eb315a1d2bc8
Badru Ndawula <badrundawula24@gmail.com>
2023-12-21 21:51:38 +0300

    add main review changes

--- main.rb
+++ main.rb
@@ -1,48 +1,49 @@
-# main.rb
+require_relative 'app'
 
-require_relative 'person'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
+def all_options
+  puts "\nPlease choose an option by enterin a number:\n
+1 - List all Books\n
+2 - List all People\n
+3 - Create a Person\n
+4 - Create a Book\n
+5 - Create a Rental\n
+6 - List all rentals for a given person id\n
+7 - Exit"
+end
 
-# Define the entry point for the console app
-def main
+def prompt
+  puts 'Welcome to School Library App!'
   loop do
-    display_menu
+    all_options
+    option = gets.chomp.to_i
+    break if option == 7
 
-    choice = gets.chomp.to_i
-
-    case choice
-    when 1
-      run_decorator_example
-    when 2
-      puts 'Exiting the app. Goodbye!'
-      break
-    else
-      puts 'Invalid choice. Please try again.'
-    end
+    call_option(option)
   end
 end
 
-# Displays the menu options to the user
-def display_menu
-  puts '------ Library App Menu ------'
-  puts '1. Run Decorator Example'
-  puts '2. Quit'
-  print 'Enter your choice: '
+def call_option(option)
+  case option
+  when 1
+    list_books
+  when 2
+    list_people
+  when 3
+    create_person
+  when 4
+    create_book
+  when 5
+    create_rental
+  when 6
+    list_rentals
+  else
+    puts 'Choose between 1 - 7'
+  end
 end
 
-# Run the example with decorators
-# Run the example with decorators
-def run_decorator_example
-  person = Person.new(age: 22, name: 'maximilianus')
-  puts "Original Name: #{person.correct_name}"
-
-  capitalized_person = CapitalizeDecorator.new(person)
-  puts "Capitalized Name: #{capitalized_person.correct_name}"
-
-  capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-  puts "Capitalized and Trimmed Name: #{capitalized_trimmed_person.correct_name}"
+def main
+  app = App.new
+  app.run
 end
 
-# Invoke the main method to start the console app
 main
