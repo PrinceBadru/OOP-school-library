c90605d1f9c3741bda5cbcebe12194a38942c208
Badru Ndawula <badrundawula24@gmail.com>
2023-12-21 21:49:50 +0300

    add book review changes

--- book.rb
+++ book.rb
@@ -1,20 +1,15 @@
-# book.rb
-
+require_relative 'rental'
 class Book
   attr_accessor :title, :author
-
-  @all_books = [] # Class instance variable to store all books
+  attr_reader :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
-    self.class.all << self # Add the book to the list when created
+    @rentals = []
   end
 
-  # Class method to retrieve all books
-  def self.all
-    @all_books
+  def add_rental(person, date)
+    Rental.new(date, self, person)
   end
-
-  # Additional methods, if needed
 end
